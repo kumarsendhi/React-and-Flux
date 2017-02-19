2f3ca2df7908040d7dca66e465d86f86da6cdd98
Kumar Krishnamurthy <Kumar Krishnamurthy>
2017-02-20 02:04:13 +0530

    react forms final commit

---
+++ React Forms/step 2/psadmin/src/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var routes = require('./routes');
+
+Router.run(routes, function(Handler){
+//Router.run(routes, Router.HistoryLocation, function(Handler){
+    React.render(<Handler />, document.getElementById('app'));
+});
+ 
+
+ 
+//React.render(<Home />, document.getElementById('app'));
